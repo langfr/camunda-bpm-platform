5532f797cf7778d730049a139382b279ae955905
Frank Langelage <Frank.Langelage@osnanet.de>
2018-02-25 20:49:14 +0100

    IBM Informix support (update IV).

--- engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.informix.create.identity.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.informix.create.identity.sql
@@ -1,4 +1,4 @@
-create table if not exists ACT_ID_GROUP (
+create table ACT_ID_GROUP (
   ID_ varchar(64) not null,
   REV_ integer,
   NAME_ varchar(255),
@@ -6,13 +6,13 @@ create table if not exists ACT_ID_GROUP (
   primary key (ID_)
 );
 
-create table if not exists ACT_ID_MEMBERSHIP (
+create table ACT_ID_MEMBERSHIP (
   USER_ID_ varchar(64) not null,
   GROUP_ID_ varchar(64) not null,
   primary key (USER_ID_, GROUP_ID_)
 );
 
-create table if not exists ACT_ID_USER (
+create table ACT_ID_USER (
   ID_ varchar(64) not null,
   REV_ integer,
   FIRST_ varchar(255),
@@ -20,11 +20,13 @@ create table if not exists ACT_ID_USER (
   EMAIL_ varchar(255),
   PWD_ varchar(255),
   SALT_ varchar(255),
+  LOCK_EXP_TIME_ datetime year to fraction(5),
+  ATTEMPTS_ integer,
   PICTURE_ID_ varchar(64),
   primary key (ID_)
 );
 
-create table if not exists ACT_ID_INFO (
+create table ACT_ID_INFO (
   ID_ varchar(64) not null,
   REV_ integer,
   USER_ID_ varchar(64),
@@ -36,14 +38,14 @@ create table if not exists ACT_ID_INFO (
   primary key (ID_)
 );
 
-create table if not exists ACT_ID_TENANT (
+create table ACT_ID_TENANT (
   ID_ varchar(64) not null,
   REV_ integer,
   NAME_ varchar(255),
   primary key (ID_)
 );
 
-create table if not exists ACT_ID_TENANT_MEMBER (
+create table ACT_ID_TENANT_MEMBER (
   ID_ varchar(64) not null,
   TENANT_ID_ varchar(64) not null,
   USER_ID_ varchar(64),
@@ -53,21 +55,13 @@ create table if not exists ACT_ID_TENANT_MEMBER (
 
 alter table ACT_ID_MEMBERSHIP 
   add constraint foreign key (GROUP_ID_)
-  references ACT_ID_GROUP (ID_)
+  references ACT_ID_GROUP(ID_)
   constraint ACT_FK_MEMB_GROUP;
 
 alter table ACT_ID_MEMBERSHIP 
   add constraint foreign key (USER_ID_)
-  references ACT_ID_USER (ID_)
+  references ACT_ID_USER(ID_)
   constraint ACT_FK_MEMB_USER;
-
-alter table ACT_ID_TENANT_MEMBER
-  add constraint unique (TENANT_ID_, USER_ID_)
-  constraint ACT_UNIQ_TENANT_MEMB_USER;
-
-alter table ACT_ID_TENANT_MEMBER
-  add constraint unique (TENANT_ID_, GROUP_ID_)
-  constraint ACT_UNIQ_TENANT_MEMB_GROUP;
 
 alter table ACT_ID_TENANT_MEMBER
   add constraint foreign key (TENANT_ID_)
@@ -83,3 +77,6 @@ alter table ACT_ID_TENANT_MEMBER
   add constraint foreign key (GROUP_ID_)
   references ACT_ID_GROUP
   constraint ACT_FK_TENANT_MEMB_GROUP;
+
+create unique index ACT_UNIQ_TENANT_MEMB_USER on ACT_ID_TENANT_MEMBER(TENANT_ID_,ACT_FCT_USER_ID_OR_ID_(USER_ID_,ID_));
+create unique index ACT_UNIQ_TENANT_MEMB_GROUP on ACT_ID_TENANT_MEMBER(TENANT_ID_,ACT_FCT_GROUP_ID_OR_ID_(GROUP_ID_,ID_));
