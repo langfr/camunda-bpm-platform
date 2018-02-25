b8b3e94b6c892071c696000ba1d1839f562aa94b
Frank Langelage <Frank.Langelage@osnanet.de>
2018-02-25 20:49:14 +0100

    IBM Informix support (update IV).

--- engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.informix.create.case.history.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.informix.create.case.history.sql
@@ -1,4 +1,4 @@
-create table if not exists ACT_HI_CASEINST (
+create table ACT_HI_CASEINST (
   ID_ varchar(64) not null,
   CASE_INST_ID_ varchar(64) not null,
   BUSINESS_KEY_ varchar(255),
@@ -11,11 +11,12 @@ create table if not exists ACT_HI_CASEINST (
   SUPER_CASE_INSTANCE_ID_ varchar(64),
   SUPER_PROCESS_INSTANCE_ID_ varchar(64),
   TENANT_ID_ varchar(64),
-  primary key (ID_),
-  unique (CASE_INST_ID_)
+  primary key (ID_)
 );
 
-create table if not exists ACT_HI_CASEACTINST (
+alter table ACT_HI_CASEINST add constraint unique(CASE_INST_ID_) constraint CASE_INST_ID_;
+
+create table ACT_HI_CASEACTINST (
   ID_ varchar(64) not null,
   PARENT_ACT_INST_ID_ varchar(64),
   CASE_DEF_ID_ varchar(64) not null,
@@ -35,13 +36,11 @@ create table if not exists ACT_HI_CASEACTINST (
   primary key (ID_)
 );
 
-create index if not exists ACT_IDX_HI_CAS_I_CLOSE on ACT_HI_CASEINST(CLOSE_TIME_);
-create index if not exists ACT_IDX_HI_CAS_I_BUSKEY on ACT_HI_CASEINST(BUSINESS_KEY_);
-create index if not exists ACT_IDX_HI_CAS_I_TENANT_ID on ACT_HI_CASEINST(TENANT_ID_);
-create index if not exists ACT_IDX_HI_CAS_I_CASE_DEF_ID on ACT_HI_CASEINST(CASE_DEF_ID_);
 
-create index if not exists ACT_IDX_HI_CAS_A_I_CREATE on ACT_HI_CASEACTINST(CREATE_TIME_);
-create index if not exists ACT_IDX_HI_CAS_A_I_END on ACT_HI_CASEACTINST(END_TIME_);
-create index if not exists ACT_IDX_HI_CAS_A_I_COMP on ACT_HI_CASEACTINST(CASE_ACT_ID_, END_TIME_, ID_);
-create index if not exists ACT_IDX_HI_CAS_A_I_TENANT_ID on ACT_HI_CASEACTINST(TENANT_ID_);
-create index if not exists ACT_IDX_HI_CAS_A_I_CASE_INST_ID on ACT_HI_CASEACTINST(CASE_INST_ID_);
+create index ACT_IDX_HI_CAS_I_CLOSE on ACT_HI_CASEINST(CLOSE_TIME_);
+create index ACT_IDX_HI_CAS_I_BUSKEY on ACT_HI_CASEINST(BUSINESS_KEY_);
+create index ACT_IDX_HI_CAS_I_TENANT_ID on ACT_HI_CASEINST(TENANT_ID_);
+create index ACT_IDX_HI_CAS_A_I_CREATE on ACT_HI_CASEACTINST(CREATE_TIME_);
+create index ACT_IDX_HI_CAS_A_I_END on ACT_HI_CASEACTINST(END_TIME_);
+create index ACT_IDX_HI_CAS_A_I_COMP on ACT_HI_CASEACTINST(CASE_ACT_ID_, END_TIME_, ID_);
+create index ACT_IDX_HI_CAS_A_I_TENANT_ID on ACT_HI_CASEACTINST(TENANT_ID_);
