55dfe36d3bbf2c91fa4cfc203a40d5c77cf95e60
Frank Langelage <Frank.Langelage@osnanet.de>
2018-02-25 20:49:14 +0100

    IBM Informix support (update IV).

--- engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.informix.drop.identity.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.informix.drop.identity.sql
@@ -1,6 +1,9 @@
-drop table if exists ACT_ID_TENANT_MEMBER;
-drop table if exists ACT_ID_TENANT;
-drop table if exists ACT_ID_INFO;
-drop table if exists ACT_ID_GROUP;
-drop table if exists ACT_ID_MEMBERSHIP;
-drop table if exists ACT_ID_USER;
+drop table ACT_ID_TENANT_MEMBER;
+drop table ACT_ID_TENANT;
+drop table ACT_ID_INFO;
+drop table ACT_ID_GROUP;
+drop table ACT_ID_MEMBERSHIP;
+drop table ACT_ID_USER;
+
+drop function ACT_FCT_USER_ID_OR_ID_;
+drop function ACT_FCT_GROUP_ID_OR_ID_;
