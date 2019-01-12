3280edca104cd543a1eb8388681080b329934b3a
Frank Langelage <Frank.Langelage@osnanet.de>
2019-01-12 22:21:43 +0100

    IBM Informix support (update VI).

--- engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.informix.create.history.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.informix.create.history.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright © 2012 - 2018 camunda services GmbH and various authors (info@camunda.com)
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 create table ACT_HI_PROCINST (
   ID_ varchar(64) not null,
   PROC_INST_ID_ varchar(64) not null,
@@ -12,7 +28,7 @@ create table ACT_HI_PROCINST (
   START_ACT_ID_ varchar(255),
   END_ACT_ID_ varchar(255),
   SUPER_PROCESS_INSTANCE_ID_ varchar(64),
-  ROOT_PROCESS_INSTANCE_ID_ varchar(64),
+  ROOT_PROC_INST_ID_ varchar(64),
   SUPER_CASE_INSTANCE_ID_ varchar(64),
   CASE_INST_ID_ varchar(64),
   DELETE_REASON_ lvarchar(4000),
@@ -28,6 +44,7 @@ create table ACT_HI_ACTINST (
   PARENT_ACT_INST_ID_ varchar(64),
   PROC_DEF_KEY_ varchar(255),
   PROC_DEF_ID_ varchar(64) not null,
+  ROOT_PROC_INST_ID_ varchar(64),
   PROC_INST_ID_ varchar(64) not null,
   EXECUTION_ID_ varchar(64) not null,
   ACT_ID_ varchar(255) not null,
@@ -43,6 +60,7 @@ create table ACT_HI_ACTINST (
   ACT_INST_STATE_ integer,
   SEQUENCE_COUNTER_ bigint,
   TENANT_ID_ varchar(64),
+  REMOVAL_TIME_ datetime year to fraction(5),
   primary key (ID_)
 );
 
@@ -51,6 +69,7 @@ create table ACT_HI_TASKINST (
   TASK_DEF_KEY_ varchar(255),
   PROC_DEF_KEY_ varchar(255),
   PROC_DEF_ID_ varchar(64),
+  ROOT_PROC_INST_ID_ varchar(64),
   PROC_INST_ID_ varchar(64),
   EXECUTION_ID_ varchar(64),
   CASE_DEF_KEY_ varchar(255),
@@ -71,6 +90,7 @@ create table ACT_HI_TASKINST (
   DUE_DATE_ datetime year to fraction(5),
   FOLLOW_UP_DATE_ datetime year to fraction(5),
   TENANT_ID_ varchar(64),
+  REMOVAL_TIME_ datetime year to fraction(5),
   primary key (ID_)
 );
 
@@ -78,6 +98,7 @@ create table ACT_HI_VARINST (
   ID_ varchar(64) not null,
   PROC_DEF_KEY_ varchar(255),
   PROC_DEF_ID_ varchar(64),
+  ROOT_PROC_INST_ID_ varchar(64),
   PROC_INST_ID_ varchar(64),
   EXECUTION_ID_ varchar(64),
   ACT_INST_ID_ varchar(64),
@@ -97,6 +118,7 @@ create table ACT_HI_VARINST (
   TEXT2_ lvarchar(4000),
   TENANT_ID_ varchar(64),
   STATE_ varchar(20),
+  REMOVAL_TIME_ datetime year to fraction(5),
   primary key (ID_)
 );
 
@@ -105,6 +127,7 @@ create table ACT_HI_DETAIL (
   TYPE_ varchar(255) not null,
   PROC_DEF_KEY_ varchar(255),
   PROC_DEF_ID_ varchar(64),
+  ROOT_PROC_INST_ID_ varchar(64),
   PROC_INST_ID_ varchar(64),
   EXECUTION_ID_ varchar(64),
   CASE_DEF_KEY_ varchar(255),
@@ -126,6 +149,7 @@ create table ACT_HI_DETAIL (
   SEQUENCE_COUNTER_ bigint,
   TENANT_ID_ varchar(64),
   OPERATION_ID_ varchar(64),
+  REMOVAL_TIME_ datetime year to fraction(5),
   primary key (ID_)
 );
 
@@ -136,11 +160,13 @@ create table ACT_HI_IDENTITYLINK (
   USER_ID_ varchar(255),
   GROUP_ID_ varchar(255),
   TASK_ID_ varchar(64),
+  ROOT_PROC_INST_ID_ varchar(64),
   PROC_DEF_ID_ varchar(64),
   OPERATION_TYPE_ varchar(64),
   ASSIGNER_ID_ varchar(64),
   PROC_DEF_KEY_ varchar(255),
   TENANT_ID_ varchar(64),
+  REMOVAL_TIME_ datetime year to fraction(5),
   primary key (ID_)
 );
 
@@ -150,11 +176,13 @@ create table ACT_HI_COMMENT (
   TIME_ datetime year to fraction(5) not null,
   USER_ID_ varchar(255),
   TASK_ID_ varchar(64),
+  ROOT_PROC_INST_ID_ varchar(64),
   PROC_INST_ID_ varchar(64),
   ACTION_ varchar(255),
   MESSAGE_ lvarchar(4000),
   FULL_MSG_ byte,
   TENANT_ID_ varchar(64),
+  REMOVAL_TIME_ datetime year to fraction(5),
   primary key (ID_)
 );
 
@@ -166,11 +194,13 @@ create table ACT_HI_ATTACHMENT (
   DESCRIPTION_ lvarchar(4000),
   TYPE_ varchar(255),
   TASK_ID_ varchar(64),
+  ROOT_PROC_INST_ID_ varchar(64),
   PROC_INST_ID_ varchar(64),
   URL_ lvarchar(4000),
   CONTENT_ID_ varchar(64),
   TENANT_ID_ varchar(64),
   CREATE_TIME_ datetime year to fraction(5),
+  REMOVAL_TIME_ datetime year to fraction(5),
   primary key (ID_)
 );
 
@@ -179,6 +209,7 @@ create table ACT_HI_OP_LOG (
   DEPLOYMENT_ID_ varchar(64),
   PROC_DEF_ID_ varchar(64),
   PROC_DEF_KEY_ varchar(255),
+  ROOT_PROC_INST_ID_ varchar(64),
   PROC_INST_ID_ varchar(64),
   EXECUTION_ID_ varchar(64),
   CASE_DEF_ID_ varchar(64),
@@ -197,6 +228,7 @@ create table ACT_HI_OP_LOG (
   ORG_VALUE_ lvarchar(4000),
   NEW_VALUE_ lvarchar(4000),
   TENANT_ID_ varchar(64),
+  REMOVAL_TIME_ datetime year to fraction(5),
   primary key (ID_)
 );
 
@@ -204,6 +236,7 @@ create table ACT_HI_INCIDENT (
   ID_ varchar(64) not null,
   PROC_DEF_KEY_ varchar(255),
   PROC_DEF_ID_ varchar(64),
+  ROOT_PROC_INST_ID_ varchar(64),
   PROC_INST_ID_ varchar(64),
   EXECUTION_ID_ varchar(64),
   CREATE_TIME_ datetime year to fraction(5) not null,
@@ -217,6 +250,7 @@ create table ACT_HI_INCIDENT (
   INCIDENT_STATE_ integer,
   TENANT_ID_ varchar(64),
   JOB_DEF_ID_ varchar(64),
+  REMOVAL_TIME_ datetime year to fraction(5),
   primary key (ID_)
 );
 
@@ -235,12 +269,14 @@ create table ACT_HI_JOB_LOG (
   JOB_DEF_CONFIGURATION_ varchar(255),
   ACT_ID_ varchar(255),
   EXECUTION_ID_ varchar(64),
+  ROOT_PROC_INST_ID_ varchar(64),
   PROCESS_INSTANCE_ID_ varchar(64),
   PROCESS_DEF_ID_ varchar(64),
   PROCESS_DEF_KEY_ varchar(255),
   DEPLOYMENT_ID_ varchar(64),
   SEQUENCE_COUNTER_ bigint,
   TENANT_ID_ varchar(64),
+  REMOVAL_TIME_ datetime year to fraction(5),
   primary key (ID_)
 );
 
@@ -257,6 +293,7 @@ create table ACT_HI_BATCH (
   CREATE_USER_ID_ varchar(255),
   START_TIME_ datetime year to fraction(5) not null,
   END_TIME_ datetime year to fraction(5),
+  REMOVAL_TIME_ datetime year to fraction(5),
   primary key (ID_)
 );
 
@@ -273,11 +310,13 @@ create table ACT_HI_EXT_TASK_LOG (
   ACT_ID_ varchar(255),
   ACT_INST_ID_ varchar(64),
   EXECUTION_ID_ varchar(64),
+  ROOT_PROC_INST_ID_ varchar(64),
   PROC_INST_ID_ varchar(64),
   PROC_DEF_ID_ varchar(64),
   PROC_DEF_KEY_ varchar(255),
   TENANT_ID_ varchar(64),
   STATE_ integer,
+  REMOVAL_TIME_ datetime year to fraction(5),
   primary key (ID_)
 );
 
@@ -287,9 +326,10 @@ create index ACT_IDX_HI_PRO_INST_TENANT_ID on ACT_HI_PROCINST(TENANT_ID_);
 create index ACT_IDX_HI_PRO_INST_PROC_DEF_KEY on ACT_HI_PROCINST(PROC_DEF_KEY_);
 create index ACT_IDX_HI_PRO_INST_PROC_TIME on ACT_HI_PROCINST(START_TIME_, END_TIME_);
 create index ACT_IDX_HI_PI_PDEFID_END_TIME on ACT_HI_PROCINST(PROC_DEF_ID_, END_TIME_);
-create index ACT_IDX_HI_PRO_INST_ROOT_PI on ACT_HI_PROCINST(ROOT_PROCESS_INSTANCE_ID_);
+create index ACT_IDX_HI_PRO_INST_ROOT_PI on ACT_HI_PROCINST(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_PRO_INST_RM_TIME on ACT_HI_PROCINST(REMOVAL_TIME_);
 
+create index ACT_IDX_HI_ACTINST_ROOT_PI on ACT_HI_ACTINST(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_ACT_INST_START on ACT_HI_ACTINST(START_TIME_);
 create index ACT_IDX_HI_ACT_INST_END on ACT_HI_ACTINST(END_TIME_);
 create index ACT_IDX_HI_ACT_INST_PROCINST on ACT_HI_ACTINST(PROC_INST_ID_, ACT_ID_);
@@ -298,12 +338,18 @@ create index ACT_IDX_HI_ACT_INST_STATS on ACT_HI_ACTINST(PROC_DEF_ID_, PROC_INST
 create index ACT_IDX_HI_ACT_INST_TENANT_ID on ACT_HI_ACTINST(TENANT_ID_);
 create index ACT_IDX_HI_ACT_INST_PROC_DEF_KEY on ACT_HI_ACTINST(PROC_DEF_KEY_);
 create index ACT_IDX_HI_AI_PDEFID_END_TIME on ACT_HI_ACTINST(PROC_DEF_ID_, END_TIME_);
+create index ACT_IDX_HI_ACT_INST_RM_TIME on ACT_HI_ACTINST(REMOVAL_TIME_);
 
+create index ACT_IDX_HI_TASKINST_ROOT_PI on ACT_HI_TASKINST(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_TASK_INST_TENANT_ID on ACT_HI_TASKINST(TENANT_ID_);
 create index ACT_IDX_HI_TASK_INST_PROC_DEF_KEY on ACT_HI_TASKINST(PROC_DEF_KEY_);
 create index ACT_IDX_HI_TASKINST_PROCINST on ACT_HI_TASKINST(PROC_INST_ID_);
 create index ACT_IDX_HI_TASKINSTID_PROCINST on ACT_HI_TASKINST(ID_,PROC_INST_ID_);
+create index ACT_IDX_HI_TASK_INST_RM_TIME on ACT_HI_TASKINST(REMOVAL_TIME_);
+create index ACT_IDX_HI_TASK_INST_START on ACT_HI_TASKINST(START_TIME_);
+create index ACT_IDX_HI_TASK_INST_END on ACT_HI_TASKINST(END_TIME_);
 
+create index ACT_IDX_HI_DETAIL_ROOT_PI on ACT_HI_DETAIL(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_DETAIL_PROC_INST on ACT_HI_DETAIL(PROC_INST_ID_);
 create index ACT_IDX_HI_DETAIL_ACT_INST on ACT_HI_DETAIL(ACT_INST_ID_);
 create index ACT_IDX_HI_DETAIL_CASE_INST on ACT_HI_DETAIL(CASE_INST_ID_);
@@ -314,44 +360,65 @@ create index ACT_IDX_HI_DETAIL_TASK_ID on ACT_HI_DETAIL(TASK_ID_);
 create index ACT_IDX_HI_DETAIL_TENANT_ID on ACT_HI_DETAIL(TENANT_ID_);
 create index ACT_IDX_HI_DETAIL_PROC_DEF_KEY on ACT_HI_DETAIL(PROC_DEF_KEY_);
 create index ACT_IDX_HI_DETAIL_BYTEAR on ACT_HI_DETAIL(BYTEARRAY_ID_);
+create index ACT_IDX_HI_DETAIL_RM_TIME on ACT_HI_DETAIL(REMOVAL_TIME_);
+create index ACT_IDX_HI_DETAIL_TASK_BYTEAR on ACT_HI_DETAIL(BYTEARRAY_ID_, TASK_ID_);
 
+create index ACT_IDX_HI_IDENT_LNK_ROOT_PI on ACT_HI_IDENTITYLINK(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_IDENT_LNK_USER on ACT_HI_IDENTITYLINK(USER_ID_);
 create index ACT_IDX_HI_IDENT_LNK_GROUP on ACT_HI_IDENTITYLINK(GROUP_ID_);
 create index ACT_IDX_HI_IDENT_LNK_TENANT_ID on ACT_HI_IDENTITYLINK(TENANT_ID_);
 create index ACT_IDX_HI_IDENT_LNK_PROC_DEF_KEY on ACT_HI_IDENTITYLINK(PROC_DEF_KEY_);
 create index ACT_IDX_HI_IDENT_LINK_TASK on ACT_HI_IDENTITYLINK(TASK_ID_);
+create index ACT_IDX_HI_IDENT_LINK_RM_TIME on ACT_HI_IDENTITYLINK(REMOVAL_TIME_);
 
+create index ACT_IDX_HI_VARINST_ROOT_PI on ACT_HI_VARINST(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_PROCVAR_PROC_INST on ACT_HI_VARINST(PROC_INST_ID_);
 create index ACT_IDX_HI_PROCVAR_NAME_TYPE on ACT_HI_VARINST(NAME_, VAR_TYPE_);
 create index ACT_IDX_HI_CASEVAR_CASE_INST on ACT_HI_VARINST(CASE_INST_ID_);
 create index ACT_IDX_HI_VAR_INST_TENANT_ID on ACT_HI_VARINST(TENANT_ID_);
 create index ACT_IDX_HI_VAR_INST_PROC_DEF_KEY on ACT_HI_VARINST(PROC_DEF_KEY_);
 create index ACT_IDX_HI_VARINST_BYTEAR on ACT_HI_VARINST(BYTEARRAY_ID_);
+create index ACT_IDX_HI_VARINST_RM_TIME on ACT_HI_VARINST(REMOVAL_TIME_);
 
 create index ACT_IDX_HI_INCIDENT_TENANT_ID on ACT_HI_INCIDENT(TENANT_ID_);
 create index ACT_IDX_HI_INCIDENT_PROC_DEF_KEY on ACT_HI_INCIDENT(PROC_DEF_KEY_);
+create index ACT_IDX_HI_INCIDENT_ROOT_PI on ACT_HI_INCIDENT(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_INCIDENT_PROCINST on ACT_HI_INCIDENT(PROC_INST_ID_);
+create index ACT_IDX_HI_INCIDENT_RM_TIME on ACT_HI_INCIDENT(REMOVAL_TIME_);
 
+create index ACT_IDX_HI_JOB_LOG_ROOT_PI on ACT_HI_JOB_LOG(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_JOB_LOG_PROCINST on ACT_HI_JOB_LOG(PROCESS_INSTANCE_ID_);
 create index ACT_IDX_HI_JOB_LOG_PROCDEF on ACT_HI_JOB_LOG(PROCESS_DEF_ID_);
 create index ACT_IDX_HI_JOB_LOG_TENANT_ID on ACT_HI_JOB_LOG(TENANT_ID_);
 create index ACT_IDX_HI_JOB_LOG_JOB_DEF_ID on ACT_HI_JOB_LOG(JOB_DEF_ID_);
 create index ACT_IDX_HI_JOB_LOG_PROC_DEF_KEY on ACT_HI_JOB_LOG(PROCESS_DEF_KEY_);
 create index ACT_IDX_HI_JOB_LOG_EX_STACK on ACT_HI_JOB_LOG(JOB_EXCEPTION_STACK_ID_);
+create index ACT_IDX_HI_JOB_LOG_RM_TIME on ACT_HI_JOB_LOG(REMOVAL_TIME_);
 
+create index ACT_HI_BAT_RM_TIME on ACT_HI_BATCH(REMOVAL_TIME_);
+
+create index ACT_HI_EXT_TASK_LOG_ROOT_PI on ACT_HI_EXT_TASK_LOG(ROOT_PROC_INST_ID_);
 create index ACT_HI_EXT_TASK_LOG_PROCINST on ACT_HI_EXT_TASK_LOG(PROC_INST_ID_);
 create index ACT_HI_EXT_TASK_LOG_PROCDEF on ACT_HI_EXT_TASK_LOG(PROC_DEF_ID_);
 create index ACT_HI_EXT_TASK_LOG_PROC_DEF_KEY on ACT_HI_EXT_TASK_LOG(PROC_DEF_KEY_);
 create index ACT_HI_EXT_TASK_LOG_TENANT_ID on ACT_HI_EXT_TASK_LOG(TENANT_ID_);
 create index ACT_IDX_HI_EXTTASKLOG_ERRORDET on ACT_HI_EXT_TASK_LOG(ERROR_DETAILS_ID_);
+create index ACT_HI_EXT_TASK_LOG_RM_TIME on ACT_HI_EXT_TASK_LOG(REMOVAL_TIME_);
 
+create index ACT_IDX_HI_OP_LOG_ROOT_PI on ACT_HI_OP_LOG(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_OP_LOG_PROCINST on ACT_HI_OP_LOG(PROC_INST_ID_);
 create index ACT_IDX_HI_OP_LOG_PROCDEF on ACT_HI_OP_LOG(PROC_DEF_ID_);
 create index ACT_IDX_HI_OP_LOG_TASK on ACT_HI_OP_LOG(TASK_ID_);
+create index ACT_IDX_HI_OP_LOG_RM_TIME on ACT_HI_OP_LOG(REMOVAL_TIME_);
+create index ACT_IDX_HI_OP_LOG_TIMESTAMP on ACT_HI_OP_LOG(TIMESTAMP_);
 
 create index ACT_IDX_HI_ATTACHMENT_CONTENT on ACT_HI_ATTACHMENT(CONTENT_ID_);
+create index ACT_IDX_HI_COMMENT_ROOT_PI on ACT_HI_COMMENT(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_ATTACHMENT_PROCINST on ACT_HI_ATTACHMENT(PROC_INST_ID_);
 create index ACT_IDX_HI_ATTACHMENT_TASK on ACT_HI_ATTACHMENT(TASK_ID_);
+create index ACT_IDX_HI_ATTACHMENT_RM_TIME on ACT_HI_ATTACHMENT(REMOVAL_TIME_);
 
 create index ACT_IDX_HI_COMMENT_TASK on ACT_HI_COMMENT(TASK_ID_);
+create index ACT_IDX_HI_ATTACHMENT_ROOT_PI on ACT_HI_ATTACHMENT(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_COMMENT_PROCINST on ACT_HI_COMMENT(PROC_INST_ID_);
+create index ACT_IDX_HI_COMMENT_RM_TIME on ACT_HI_COMMENT(REMOVAL_TIME_);
