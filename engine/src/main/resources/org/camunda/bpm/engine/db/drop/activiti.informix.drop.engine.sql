a576cd0ef8d65e286cf2a012b77eca31f5433766
Frank Langelage <Frank.Langelage@outlook.de>
2021-09-25 12:46:56 +0200

    IBM Informix support (update VII).

--- engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.informix.drop.engine.sql
+++ engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.informix.drop.engine.sql
@@ -1,5 +1,6 @@
 drop table ACT_GE_PROPERTY;
 drop table ACT_GE_BYTEARRAY;
+drop table ACT_GE_SCHEMA_LOG;
 drop table ACT_RE_DEPLOYMENT;
 drop table ACT_RE_PROCDEF;
 drop table ACT_RU_IDENTITYLINK;
@@ -13,6 +14,7 @@ drop table ACT_RU_INCIDENT;
 drop table ACT_RU_AUTHORIZATION;
 drop table ACT_RU_FILTER;
 drop table ACT_RU_METER_LOG;
+drop table ACT_RU_TASK_METER_LOG;
 drop table ACT_RU_EXT_TASK;
 drop table ACT_RU_BATCH;
 
